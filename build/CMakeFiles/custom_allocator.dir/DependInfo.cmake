3365c04d3451dfedb436047d059e0b1796959cdd
rxhxl-sxngh <rsingh032503@gmail.com>
2025-03-30 14:38:01 -0500

    executables

---
+++ build/CMakeFiles/custom_allocator.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rsing/OSProj2-main/src/alloc.c" "CMakeFiles/custom_allocator.dir/src/alloc.c.o" "gcc" "CMakeFiles/custom_allocator.dir/src/alloc.c.o.d"
+  "/home/rsing/OSProj2-main/src/main.c" "CMakeFiles/custom_allocator.dir/src/main.c.o" "gcc" "CMakeFiles/custom_allocator.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/custom_allocator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/custom_allocator.dir/src/alloc.c.o"
+  "CMakeFiles/custom_allocator.dir/src/alloc.c.o.d"
+  "CMakeFiles/custom_allocator.dir/src/main.c.o"
+  "CMakeFiles/custom_allocator.dir/src/main.c.o.d"
+  "custom_allocator"
+  "custom_allocator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/custom_allocator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
